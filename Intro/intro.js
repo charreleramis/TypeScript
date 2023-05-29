f491e467eb8ca8729833ce650b6fd64039371759
ceroadr <ce@roadr.com>
2023-05-29 12:00:19 +0800

    commit

---
+++ Intro/intro.js
@@ -0,0 +1,7 @@
+var obj = { width: 10, height: 15 };
+var area = obj.width * obj.height;
+var a;
+a = 'chare';
+console.log('hello world');
+console.log(area);
+console.log(a);
---
+++ Intro/intro.ts
@@ -0,0 +1,9 @@
+const obj = { width: 10, height: 15 };
+const area = obj.width * obj.height;
+
+let a: number;
+a = 10;
+
+console.log('hello world');
+console.log(area);
+console.log(a);
